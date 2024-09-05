6c052ecf20c86db19e77737d980f198a572c5b83
Olivier MOULIN <olivier.moulin@Digital-Grenoble.local>
2024-09-05 10:43:32 +0200

    exo-4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,43 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+// console.log(secretMessage.length);
+
+
+function removeLastElement(arr){
+    arr.pop();
+}
+removeLastElement(secretMessage);
+// console.log(secretMessage);
+
+function addElement(arr){
+    arr.push('to');
+    arr.push('program');
+}
+addElement(secretMessage);
+// console.log(secretMessage);
+
+secretMessage[6]='right';
+// console.log(secretMessage);
+
+function removeFirstElement(arr){
+    arr.shift();
+}
+removeFirstElement(secretMessage);
+// console.log(secretMessage);
+
+function addFirstElement(arr){
+    arr.unshift('Programming');
+}
+addFirstElement(secretMessage);
+// console.log(secretMessage);
+
+function replaceByKnow(arr){
+    arr.splice(5, 5, 'know');    
+}
+replaceByKnow(secretMessage);
+// console.log(secretMessage);
+
+console.log(secretMessage.join(' '));
+
